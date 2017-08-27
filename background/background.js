ce805c82d1580aef6d483e672267cc5481b588e0
yanske1 <yansongke1@gmail.com>
2017-08-27 03:00:18 -0400

    Added data store

--- background/background.js
+++ background/background.js
@@ -1,11 +1,22 @@
 var once = false;
 window.addEventListener("load", function () {
     if ( once == true ) { return; }
+
+    if (chrome.storage.sync.get('got', function(items){
+        if(items.got == true){
+            return;
+        }
+    }));
+
     setTimeout(function(){
-        if(confirm( 'A new episode of Game of Thrones aired on August 20, 2017! Add spoilers protection?' )){
-            alert("yes");
+        if(confirm( 'A new episode of Game of Thrones aired on August 20, 2017! Add spoilers protection to Game of Thrones now?' )){
+            chrome.storage.sync.set({'got': true}, function() {
+                alert("saved t");
+              });
         } else {
-            alert("no");
+            chrome.storage.sync.set({'got': false}, function() {
+                alert("saved f");
+              })
         }
     }, 2000);
     once = true;
