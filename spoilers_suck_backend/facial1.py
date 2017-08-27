a7b4de986e045c95d52890f7c72378a78b53d871
yanske1 <yansongke1@gmail.com>
2017-08-26 21:46:06 -0400

    Added images to our base

--- spoilers_suck_backend/facial1.py
+++ spoilers_suck_backend/facial1.py
@@ -1,7 +1,6 @@
 import requests
 import base64
 
-image_paths = ['jon_snow/jon-snow.jpg', 'jon_snow/jon-snow1.jpg', 'jon_snow/jon-snow2.jpg']
 base_url = 'https://api.kairos.com/'
 auth_headers = {
         'app_id': '4f9705ff',
@@ -20,13 +19,6 @@ def enroll_face(subject_id, gallery_name, url=None, file = None, base64_image_co
     response = requests.post(base_url+'enroll', json=payload, headers = auth_headers)
     json_response = response.json()
 
-    return json_response
-
-def detect_face(url=None, file=None, additional_args={}):
-    payload = build_payload(url, file, additional_args)
-    response = requests.post(base_url+'detect', json = payload, headers = auth_headers)
-    json_response = response.json()
-    
     return json_response
 
 def build_recognize_payload(gallery_name, url, file, additional_args):
@@ -59,22 +51,18 @@ def build_enroll_payload(subject_id, gallery_name, url, file, imgframe, multiple
 
     return dict(required_fields, **additional_args)
 
-def build_payload(url, file, additional_args):
-    if file is not None:
-        image = extract_base64_content(file)
-    else:
-        image = url
-    
-    required_fields = {
-        'image': image
-    }
-
-    return dict(required_fields, **additional_args)
-
 def extract_base64_content(image_path):
     with open(image_path, 'rb') as fp:
         return base64.b64encode(fp.read()).decode('ascii')
 
-#print enroll_face(file=image_paths[1], subject_id='Jon Snow', gallery_name='1')
+#add the following data to gallery name
+#print enroll_face(file='jon_snow/jon-snow1.jpg', subject_id='Jon Snow', gallery_name='1')
+#print enroll_face(file='arya_stark/arya-stark' + str(i)+'.jpg', subject_id='Arya Stark', gallery_name='1')
+#print enroll_face(file='cersei_lannister/cersei-lanister' + str(i)+'.jpg', subject_id='Arya Stark', gallery_name='1')
+#print enroll_face(file='daenerys_targaryen/daenerys-targaryen' + str(i)+'.jpg', subject_id='Arya Stark', gallery_name='1')
+#print enroll_face(file='gregor_clegane/gregor-clegane' + str(i)+'.jpg', subject_id='Arya Stark', gallery_name='1')
+#print enroll_face(file='sansa_stark/sansa-stark' + str(i)+'.jpg', subject_id='Arya Stark', gallery_name='1')
+#print enroll_face(file='tyrion_lannister/tyrion_lannister' + str(i)+'.jpg', subject_id='Arya Stark', gallery_name='1')
 
-print recognize_face(file=image_paths[2], gallery_name='1')
+
+print recognize_face(url='http://pixel.nymag.com/imgs/daily/vulture/2015/09/15/15-kit-harington-got-snow.w529.h529.jpg', gallery_name='1')
