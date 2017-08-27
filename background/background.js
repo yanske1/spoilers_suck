d7f02547c978c29935e94bee8752ac218871e187
yanske1 <yansongke1@gmail.com>
2017-08-27 03:42:47 -0400

    debug script

--- background/background.js
+++ background/background.js
@@ -5,18 +5,16 @@ window.addEventListener("load", function () {
     if (chrome.storage.sync.get('got', function(items){
         if(items.got == true){
             return;
+        } else {
+            alert ("false");
         }
     }));
 
     setTimeout(function(){
         if(confirm( 'A new episode of Game of Thrones aired on August 20, 2017! Add spoilers protection to Game of Thrones now?' )){
-            chrome.storage.sync.set({'got': true}, function() {
-                alert("saved t");
-              });
+            chrome.storage.sync.set({'got': true}, function() {});
         } else {
-            chrome.storage.sync.set({'got': false}, function() {
-                alert("saved f");
-              })
+            chrome.storage.sync.set({'got': false}, function() {})
         }
     }, 2000);
     once = true;
---
+++ public/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('got').addEventListener('click', function(){
+    alert("hi!");
+  });
+}, false);
